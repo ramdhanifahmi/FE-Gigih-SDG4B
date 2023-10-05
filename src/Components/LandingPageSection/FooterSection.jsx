e78ea51d99956d1c1352eaecb09da79bc53ecd80
ftaufik.ros <ftaufik.ros@gmail.com>
2023-10-06 00:00:30 +0700

    finished footer section

---
+++ src/Components/LandingPageSection/FooterSection.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+export const FooterSection = () => {
+  return (
+    <div className="p-2 bg-dark text-white text-center position-relative">
+        <Container>
+            <p className="lead">Copyright &copy; 2023 Generasi GIGIH SDG4B</p>
+        </Container>
+      </div>
+  )
+}
